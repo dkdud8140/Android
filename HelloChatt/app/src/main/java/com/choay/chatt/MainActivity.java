6dd2f0dae0e54c4210ea979f4bd6026d7b2107c8
dkdud8140 <dkdud8140@gmail.com>
2021-08-04 11:53:43 +0900

    0803

--- HelloChatt/app/src/main/java/com/choay/chatt/MainActivity.java
+++ HelloChatt/app/src/main/java/com/choay/chatt/MainActivity.java
@@ -7,9 +7,12 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.choay.chatt.adapter.chattAdapter;
 import com.choay.chatt.model.chatt;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,11 +28,19 @@ public class MainActivity extends AppCompatActivity {
     private chattAdapter chattAdapter;
     private List<chatt> chattList;
 
+    //fireBase 와 연결하는 Connection을 위한 객체 선언
+    private DatabaseReference dbRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        FirebaseDatabase dbConn = FirebaseDatabase.getInstance();
+        // 사용할 table 생성
+        // realtimeDatabase에서는 table을 path라는 개념으로 부른다
+        dbRef = dbConn.getReference("chatting");
 
         txt_msg = findViewById(R.id.txt_msg);
         btn_send = findViewById(R.id.btn_send);
@@ -64,6 +75,37 @@ public class MainActivity extends AppCompatActivity {
         // 3.RecyclerView의 데이터를 표현하는데 사용할 레이아웃 매너저 설정하기
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
         chat_list_view.setLayoutManager(layoutManager);
+
+
+        /*
+        EditBox에 메시지를 입력하고 Send 버튼을 클릭했을 때 할 일 지정하기
+
+        EditBox에 메시지를 입력하고 Send를 하면
+        FireBase의 Realtime DatabASE에 insert하기기
+        */
+        btn_send.setOnClickListener(view->{
+
+            //EditBox에 입력 된 문자열을 추출하여 문자열 변수에 담기
+            String msg = txt_msg.getText().toString();
+            if(msg != null && !msg.isEmpty()) {
+
+                String toastMsg = String.format("메시지 : %s",msg);
+                Toast.makeText(this, toastMsg, Toast.LENGTH_SHORT).show();
+
+                chatt chatVO = new chatt();
+                chatVO.setMsg(msg);
+                chatVO.setName("홍길동");
+
+                // chattList.add(chatVO);
+                // firebase의 realtime DB의 table에 데이터를 insert(push)하라
+                dbRef.push().setValue(chatVO);
+                txt_msg.setText("");
+            }
+
+        });
+
+
+
 
     }
 }
