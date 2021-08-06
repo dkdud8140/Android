23905631a1036ff9a3dcdc138e7be5769b27a0ff
dkdud8140 <dkdud8140@gmail.com>
2021-08-06 11:05:34 +0900

    Naver_ID

--- App02_library/app/src/main/java/com/choay/library/config/Naver.java
+++ App02_library/app/src/main/java/com/choay/library/config/Naver.java
@@ -2,7 +2,8 @@ package com.choay.library.config;
 
 public class Naver {
 
+    // Naver Open API 연동을 위한 ID와 PW
     public static String CLIENT_ID = "your naver client id";
-    public static String CLIENT_SECRET = "0tRMCD5oJV";
+    public static String CLIENT_SECRET = "your naver client SECRET";
 
 }
