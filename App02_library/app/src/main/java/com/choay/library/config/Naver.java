67aab7f65c4e5e3236f0dc68354acec7bce8fe5f
dkdud8140 <dkdud8140@gmail.com>
2021-08-06 10:49:41 +0900

    내도서관

---
+++ App02_library/app/src/main/java/com/choay/library/config/Naver.java
@@ -0,0 +1,8 @@
+package com.choay.library.config;
+
+public class Naver {
+
+    public static String CLIENT_ID = "your naver client id";
+    public static String CLIENT_SECRET = "0tRMCD5oJV";
+
+}
